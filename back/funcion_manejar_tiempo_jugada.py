6a56e71afb0b7fc4c097f9d41abd306a2c407d90
caroliu28 <carolinaliu173@gmail.com>
2025-04-14 16:57:47 -0300

    Agregué función para el tiempo de espera por juego

---
+++ back/funcion_manejar_tiempo_jugada.py
@@ -0,0 +1,14 @@
+from funcion_tirar_dados_inicio import tirar_dados
+
+import time
+
+def manejar_tiempo(jugador, tiempo_max=6):
+    print(f"\n{jugador['nombre']} tiene {tiempo_max} segundos para tirar los dados.")
+
+    start = time.time()
+    inicio_jugada = input("Presioná ENTER para tirar los dados")
+    end = time.time()
+
+    if end - start > tiempo_max:
+        print("Se pasó el tiempo.")
+        jugador['mano'].append(tirar_dados())
