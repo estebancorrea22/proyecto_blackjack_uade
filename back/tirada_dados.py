9c6376cad6d034ae68f616e1b3f47ad281a93686
Carolina <carolinaliu173@gmail.com>
2025-07-07 00:10:55 -0300

    Corregir blackjack para que solo cuente con 2 dados iniciales

--- back/tirada_dados.py
+++ back/tirada_dados.py
@@ -25,31 +25,6 @@ def evaluar_mano(mano):
     total = sumar_dados(mano)
     if total > 21:
         return "pasado", total
-    elif total == 21:
-        return "blackjack", total
-    else:
-        return "jugando", total
-
-
-# import random
-
-# dados_tirados=[]
-
-# sumar_dados= lambda dados: sum(dados)+(10 if 1 in dados and sum(dados) + 10 <= 21 else 1)
-
-# def tirar_dado(veces=1):
-#     nuevos_dado=[]
-#     for _ in range(veces):
-#         dado=random.randint(1, 10)
-#         nuevos_dado.append(dado)
-#     dados_tirados.extend(nuevos_dado)
-#     return nuevos_dado
-
-# def evaluar_mano(mano):
-#     total = sumar_dados(mano)
-#     if total > 21:
-#         return "pasado", total
-#     elif total == 21:
-#         return "blackjack", total
-#     else:
-#         return "jugando", total
+    if total == 21:
+        return ("blackjack" if len(mano) == 2 else "jugando"), total
+    return "jugando", total
